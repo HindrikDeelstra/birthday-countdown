f242f9989a34dc3fcfb8c75a3b84ee40e7952c8d
HindrikDeelstra <hhdeelstra@gmail.com>
2014-05-18 10:57:17 +0200

    Added comments

--- date.php
+++ date.php
@@ -6,11 +6,26 @@ ini_set('date.timezone', 'Europe/Amsterdam');
 
 include('../../medewerkers.php');
 
+/* The file 'medewerkers.php' contains an array '$people' in the form:
+** $people = array(
+** 'FirstName1' => 'Birthdate1',
+** 'FirstName2' => 'Birthdate2',
+** );
+**
+** This info might also be taken from a Database ofcourse...
+*/
+
+// Only consider birthdays coming up in a limited amount of time
 $daystolook = 31;
+
+// Create an array to be filled with data, this will be sorted later
 $list = array();
+
+// Initialize array id and collect data
 $i = 0;
 foreach($people as $name=>$date) {
     $bday = new Birthday($date);
+    // Skip entries that are more than $daystolook number of days in the future
     if($bday->getDaysToGo() > $daystolook) continue;
     $list[$i]['name'] = $name;
     $list[$i]['next'] = $bday->getNextDate();
@@ -20,12 +35,15 @@ foreach($people as $name=>$date) {
     $i++;
 }
 
+// Sort the array by number of days to next birthday
 usort($list, 'sortByDays');
 
+// Define function to sort array by number of days left
 function sortByDays($a, $b) {
     return $a['days'] - $b['days'];
 }
 
+// Print the results
 foreach ($list as $birth) {
     if($birth['istoday']){
         print("Vandaag jarig: $birth[name] ($birth[age])".PHP_EOL);
