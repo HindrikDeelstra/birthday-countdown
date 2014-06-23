eaa3bd0a29957548f3cdb2c874364f1a85544792
Hindrik Deelstra <h.deelstra@oxilion.nl>
2014-06-23 18:04:56 +0200

    Swith to DateTime objects for birthday class

--- class.birthday.php
+++ class.birthday.php
@@ -1,35 +1,37 @@
 <?php
 
-/* PHP class to obtain certain data from a given birthdate */
+/*
+** Simple PHP class to obtain next occurrence, age and days to go from a given birthdate.
+** All dates and differences are DateTime objects ad DateTimeInterfaces.
+*/
 
 class Birthday {
 
     private $nextdate;
-    private $age;
-    private $daystogo;
+    private $frombirth;
+    private $tonext;
 
     public function __construct($date) {
-        $test_date = date('Y').'-'.date('m-d', strtotime($date));
-        $this->nextdate = ($test_date >= date('Y-m-d') ? $test_date : date('Y-m-d', strtotime($test_date." +1 year")));
-        $this->age = date('Y', strtotime($this->nextdate)) - date('Y', strtotime($date));
-        $this->daystogo = abs(ceil((strtotime($this->nextdate) - time())/(60*60*24)));
+        $birthdate = new DateTime($date);
+        $today = new DateTime("today");
+        $this->nextdate = new DateTime($birthdate->format('d F')." this year");
+        $this->nextdate >= $today or $this->nextdate->modify("+1 year");
+        $this->frombirth = $birthdate->diff($this->nextdate);
+        $this->tonext = $today->diff($this->nextdate);
     }
 
     public function getNextDate() {
-        return $this->nextdate;
+        return $this->nextdate->format('d-m-Y');
     }
 
     public function getAge() {
-        return $this->age;
+        return $this->frombirth->format('%y');
     }
 
     public function getDaysToGo() {
-        return $this->daystogo;
+        return $this->tonext->format('%a');
     }
 
-    public function isToday() {
-        return ($this->nextdate == date('Y-m-d'));
-    }
 }
 
 ?>
