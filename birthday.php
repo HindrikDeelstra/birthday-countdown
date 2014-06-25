f64276b2acce31c623dc21defb119f8934c99b3d
Hindrik Deelstra <h.deelstra@oxilion.nl>
2014-06-25 13:51:42 +0200

    Readied for inclusion on webserver

--- birthday.php
+++ birthday.php
@@ -1,19 +1,12 @@
-#!/usr/bin/php
 <?php
 
-require_once('class.birthday.php');
+@require_once('class.birthday.php');
 ini_set('date.timezone', 'Europe/Amsterdam');
 
-include(__DIR__.'/../../medewerkers.php');
-
-/* The file 'medewerkers.php' contains an array '$people' in the form:
-** $people = array(
-** 'FirstName1' => 'Birthdate1',
-** 'FirstName2' => 'Birthdate2',
-** );
-**
-** This info might also be taken from a Database ofcourse...
-*/
+// Initialize $people just in case the file include fails
+$people = array();
+@include('medewerkers.php');
+@include(__DIR__.'/../../medewerkers.php');
 
 // Initialize array, data filled in as array[$DaysToGo] = (List of all names and ages)
 $list = array();
@@ -21,7 +14,7 @@ $list = array();
 // Fill the array with data
 foreach($people as $name=>$date) {
     $bday = new Birthday($date);
-    @$list[$bday->getDaysToGo()] .= $name.'('.$bday->getAge().') ';
+    @$list[$bday->getDaysToGo()] .= $name.' ('.$bday->getAge().') ';
 }
 
 // Sort the array by key = number of DaysToGo
@@ -29,29 +22,23 @@ ksort($list);
 
 // Initialize a few other settings
 $showlater = true;
-$slingers = false;
 
 // When birthdays are today or tomorrow, skip listing any later birthdays
-if(key($list) == '0') {
-    print("Vandaag jarig: ");
-    print(current($list));
+if(key($list) == 0) {
+    print('<div class="flags"></div>'.PHP_EOL);
+    print('<div class="birthday">Vandaag jarig: '.current($list).'</div>'.PHP_EOL);
     next($list);
     $showlater = false;
-    $slingers = true;
 }
-if(key($list) == '1') {
-    print("Morgen jarig: ");
-    print(current($list));
+if(key($list) == 1) {
+    print('<div class="birthday">Morgen jarig: '.current($list).'</div>'.PHP_EOL);
     next($list);
     $showlater = false;
 }
 
 // If there are no birthdays today or tomorrow, print just the next occurrence
 if($showlater) {
-    print( (current($list) ? 'Over '.key($list).' dagen jarig: '.current($list) : '' ) );
+    print( (current($list) ? '<div class="birthday">Over '.key($list).' dagen jarig: '.current($list).'</div>'.PHP_EOL : '' ) );
 }
-
-// When outputting to console, start a newline
-print PHP_EOL;
 
 ?>
