af84615720780f8eea5bed6ef6668fab2f6324a8
Hindrik Deelstra <h.deelstra@oxilion.nl>
2014-06-26 14:18:58 +0200

    Only show list when requested, only dates otherwise

--- birthday.php
+++ birthday.php
@@ -7,39 +7,6 @@ ini_set('date.timezone', 'Europe/Amsterdam');
 require_once('class.birthday.php');
 include_once('medewerkers.php');
 
-// Initialize array, data filled in as array[$DaysToGo] = (List of all names and ages)
-$list = array();
-
-// Fill the array with data
-foreach($people as $name=>$date) {
-    $bday = new Birthday($date);
-    @$list[$bday->getDaysToGo()] .= $name.' ('.$bday->getAge().') ';
-}
-
-// Sort the array by key = number of DaysToGo
-ksort($list);
-
-// Initialize misc variables
-$showlater = true;
-
-// When birthdays are today or tomorrow, skip listing any later birthdays
-if(key($list) == 0) {
-    print('<div class="flags"></div>'.PHP_EOL);
-    print('<div class="birthday">Vandaag jarig: '.current($list).'</div>'.PHP_EOL);
-    next($list);
-    $showlater = false;
-}
-if(key($list) == 1) {
-    print('<div class="birthday">Morgen jarig: '.current($list).'</div>'.PHP_EOL);
-    next($list);
-    $showlater = false;
-}
-
-// If there are no birthdays today or tomorrow, print just the next occurrence
-if($showlater) {
-    print( (current($list) ? '<div class="birthday">Over '.key($list).' dagen jarig: '.current($list).'</div>'.PHP_EOL : '' ) );
-}
-
 // Dump all birthday data on request
 if(isset($_GET['list'])) {
     $dump = array();
@@ -57,6 +24,40 @@ if(isset($_GET['list'])) {
         print('<td>'.$bday->getAge().'</td></tr>'.PHP_EOL);
     }
     print('</table>'.PHP_EOL);
+} else {
+
+    // Initialize array, data filled in as array[$DaysToGo] = (List of all names and ages)
+    $list = array();
+
+    // Fill the array with data
+    foreach($people as $name=>$date) {
+        $bday = new Birthday($date);
+        @$list[$bday->getDaysToGo()] .= $name.' ('.$bday->getAge().') ';
+    }
+
+    // Sort the array by key = number of DaysToGo
+    ksort($list);
+
+    // Initialize misc variables
+    $showlater = true;
+
+    // When birthdays are today or tomorrow, skip listing any later birthdays
+    if(key($list) == 0) {
+        print('<div class="flags"></div>'.PHP_EOL);
+        print('<div class="birthday">Vandaag jarig: '.current($list).'</div>'.PHP_EOL);
+        next($list);
+        $showlater = false;
+    }
+    if(key($list) == 1) {
+        print('<div class="birthday">Morgen jarig: '.current($list).'</div>'.PHP_EOL);
+        next($list);
+        $showlater = false;
+    }
+
+    // If there are no birthdays today or tomorrow, print just the next occurrence
+    if($showlater) {
+        print( (current($list) ? '<div class="birthday">Over '.key($list).' dagen jarig: '.current($list).'</div>'.PHP_EOL : '' ) );
+    }
 }
 
 ?>
