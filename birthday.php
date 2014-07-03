05c0d981c613e0dd45ea5c38bc540f00733d3e1d
Hindrik Deelstra <h.deelstra@oxilion.nl>
2014-07-03 12:15:14 +0200

    Add more comments, remove unneeded array declaration

--- birthday.php
+++ birthday.php
@@ -3,13 +3,17 @@
 // We're working with DateTimes, so we'll set the correct timezone first
 ini_set('date.timezone', 'Europe/Amsterdam');
 
-// Include the class and data sources
+// Include the class
 require_once('class.birthday.php');
+
+// Get birthdays from file.
+// Data-output from 'medewerkers.php' is expected as an array named $people,
+// containing First Name as keys, and their birthdate as the values:
+// $people = array('FirstName1' => 'birthdate1', 'FirstName2' => 'birthdate2', .. , 'FirstNameN' => 'birthdateN');
 include_once('medewerkers.php');
 
 // Dump all birthday data on request
 if(isset($_GET['list'])) {
-    $dump = array();
     print('<table width="450"><tr><th align="left">Naam</th>');
     print('<th align="left">Geb.datum</th>');
     print('<th align="left">Verjaardag</th>');
@@ -26,24 +30,24 @@ if(isset($_GET['list'])) {
     print('</table>'.PHP_EOL);
 } else {
 
-    // Initialize array, data filled in as array[$DaysToGo] = (List of all names and ages)
+    // Initialize list, data filled in as array[$DaysToGo] = (List of all names and ages)
     $list = array();
 
-    // Fill the array with data
+    // Fill the list with data
     foreach($people as $name=>$date) {
         $bday = new Birthday($date);
         @$list[$bday->getDaysToGo()] .= $name.' ('.$bday->getAge().') ';
     }
 
-    // Sort the array by key = number of DaysToGo
+    // Sort the list by key = number of DaysToGo
     ksort($list);
 
     // Initialize misc variables
     $showlater = true;
 
-    // When birthdays are today or tomorrow, skip listing any later birthdays
+    // If there are any birthdays today or tomorrow, show them, but not any later birthdays
     if(key($list) == 0) {
-        print('<div class="flags"></div>'.PHP_EOL);
+        print('<div class="flags"></div>'.PHP_EOL); // Show a decorative ribbon of flags
         print('<div class="birthday">Vandaag jarig: '.current($list).'</div>'.PHP_EOL);
         next($list);
         $showlater = false;
@@ -54,7 +58,7 @@ if(isset($_GET['list'])) {
         $showlater = false;
     }
 
-    // If there are no birthdays today or tomorrow, print just the next occurrence
+    // If there are no birthdays today or tomorrow, show just the first one in the list
     if($showlater) {
         print( (current($list) ? '<div class="birthday">Over '.key($list).' dagen jarig: '.current($list).'</div>'.PHP_EOL : '' ) );
     }
